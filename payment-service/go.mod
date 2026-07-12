cef93c037e8f186ec45a2798ee73ce992388f0d5
rajabhishekmaurya <rajabhishekmaurya3@gmail.com>
2026-07-12 22:10:06 +0530

    feat(payment-service): publish payment events to Kafka

--- payment-service/go.mod
+++ payment-service/go.mod
@@ -4,11 +4,15 @@ go 1.25.0
 
 require (
 	github.com/joho/godotenv v1.5.1
+	github.com/segmentio/kafka-go v0.4.51
 	google.golang.org/grpc v1.82.0
 	google.golang.org/protobuf v1.36.11
 )
 
 require (
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
 	golang.org/x/net v0.56.0 // indirect
 	golang.org/x/sys v0.46.0 // indirect
 	golang.org/x/text v0.38.0 // indirect
--- payment-service/internal/service/payment.go
+++ payment-service/internal/service/payment.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/rajabhishekmaurya/ecommerce-microservices/payment-service/internal/model"
 	pb "github.com/rajabhishekmaurya/ecommerce-microservices/payment-service/proto"
 )
 
@@ -18,10 +19,27 @@ func (s *PaymentServer) ProcessPayment(ctx context.Context, req *pb.PaymentReque
 	fmt.Println("Order ID :", req.OrderId)
 	fmt.Println("Amount   :", req.Amount)
 
+	// Generate transaction ID once
+	txnID := fmt.Sprintf("TXN-%d", time.Now().Unix())
+
+	producer := NewKafkaProducer()
+	defer producer.Close()
+
+	event := &model.PaymentEvent{
+		OrderID:       req.OrderId,
+		TransactionID: txnID,
+		Amount:        req.Amount,
+		Status:        "SUCCESS",
+	}
+
+	if err := producer.Publish(event); err != nil {
+		return nil, err
+	}
+
 	time.Sleep(500 * time.Millisecond)
 
 	return &pb.PaymentResponse{
 		Success:       true,
-		TransactionId: fmt.Sprintf("TXN-%d", time.Now().Unix()),
+		TransactionId: txnID,
 	}, nil
 }
